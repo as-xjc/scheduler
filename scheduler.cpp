008ac0e247beedb7e975b2ca145b801a212c592b
As-xjc <AzureSky.xjc@gmail.com>
2014-06-15 12:30:19 +0000

    init scheduler

---
+++ scheduler.cpp
@@ -0,0 +1,106 @@
+#include <ctime>
+#include "scheduler.hpp"
+
+namespace schedule
+{
+
+namespace 
+{
+
+void schedule_callback(uv_timer_t* handle, int status)
+{
+	schedule_t* tmp = reinterpret_cast<schedule_t*>(handle->data);
+	tmp->callback();
+
+	if (tmp->type == schedule_type::run_one) {
+		tmp->base->unschedule(tmp->id);
+	}
+}
+
+}
+
+scheduler::scheduler()
+{
+	_id = 1;
+}
+
+scheduler::~scheduler()
+{
+}
+
+void scheduler::init(uv_loop_t* loop)
+{
+	if (loop != nullptr) {
+		_loop = loop;
+	}
+}
+
+schedule_id scheduler::_schedule(uint64_t timeout, uint64_t repeat, schedule_cb& cb)
+{
+	auto tmp = std::make_shared<schedule_t>();
+	tmp->callback = cb;
+	tmp->base = this;
+	if (repeat) {
+		tmp->type = schedule_type::repeat;
+	} else {
+		tmp->type = schedule_type::run_one;
+	}
+	
+	uv_timer_init(_loop, &tmp->timer);
+
+	schedule_id newid = 0;
+	if (uv_timer_start(&tmp->timer, schedule_callback, timeout, repeat) == 0) {
+		tmp->timer.data = reinterpret_cast<void*>(tmp.get());
+		newid = _id++;
+		tmp->id = newid;
+		_schedules[newid] = tmp;
+	} 
+
+	return newid;
+}
+
+schedule_id scheduler::run_at(uint64_t target_time, schedule_cb cb, bool use_millisecond)
+{
+	std::time_t now = std::time(nullptr);
+	uint64_t timeout = 0;
+	if (use_millisecond) {
+		uint64_t _now = now*one_second;
+		if (target_time <= _now) return 0;
+
+		timeout = target_time - _now;
+	} else {
+		if (target_time <= static_cast<uint64_t>(now)) return 0;
+		timeout = (target_time-now)*one_second;
+	}
+
+	return _schedule(timeout, 0, cb);
+}
+
+schedule_id scheduler::heartbeat(uint64_t repeat, schedule_cb cb, bool start_now, bool use_millisecond)
+{
+	if (repeat <= 0) return 0;
+
+	uint64_t heartbeat = 0;
+	if (use_millisecond) {
+		heartbeat = repeat;
+	} else {
+		heartbeat = repeat*one_second;
+	}
+
+	uint64_t timeout = 0;
+	if (!start_now) timeout = heartbeat;
+
+	return _schedule(timeout, heartbeat, cb);
+}
+
+void scheduler::unschedule(schedule_id id)
+{
+	auto it = _schedules.find(id);
+	if (it == _schedules.end()) return;
+
+	auto& tmp = it->second;
+	uv_timer_stop(&(tmp->timer));
+	_schedules.erase(it);
+}
+
+}
---
+++ scheduler.hpp
@@ -0,0 +1,53 @@
+#ifndef __SCHDULE_SCHDULER__
+#define __SCHDULE_SCHDULER__
+
+#include <map>
+#include <functional>
+#include <memory>
+#include "uv.h"
+
+namespace schedule
+{
+
+const static uint64_t one_second = 1000;
+const static uint64_t one_minute = one_second*60;
+const static uint64_t one_hour = one_minute*60;
+const static uint64_t one_day = one_hour*24;
+
+typedef uint64_t schedule_id;
+typedef std::function<void()> schedule_cb;
+
+class scheduler;
+
+enum class schedule_type {repeat, run_one};
+typedef struct 
+{
+	schedule_id id;
+	schedule_type type;
+	uv_timer_t timer;
+	schedule_cb callback;
+	scheduler* base;
+}schedule_t;
+
+class scheduler 
+{
+public:
+	scheduler();
+	~scheduler();
+	void init(uv_loop_t* loop);
+
+	void unschedule(schedule_id id);
+
+	schedule_id run_at(uint64_t target_time, schedule_cb cb, bool use_millisecond = false);
+	schedule_id heartbeat(uint64_t repeat, schedule_cb cb, bool start_now = false, bool use_millisecond  = false);
+
+private:
+	schedule_id _schedule(uint64_t timeout, uint64_t repeat, schedule_cb& cb);
+	uv_loop_t* _loop;
+	schedule_id _id;
+	std::map<schedule_id, std::shared_ptr<schedule_t>> _schedules;
+};
+
+}
+
+#endif
